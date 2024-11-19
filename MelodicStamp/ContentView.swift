e02bfefb4f1822fd4bee4fffd7df9fec1be3bff4
芯梢 <aolie06@qq.com>
2024-11-19 21:49:32 +0800

    Initial Commit

---
+++ MelodicStamp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MelodicStamp
+//
+//  Created by 屈志健 on 2024/11/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MelodicStamp/MelodicStampApp.swift
@@ -0,0 +1,17 @@
+//
+//  MelodicStampApp.swift
+//  MelodicStamp
+//
+//  Created by 屈志健 on 2024/11/19.
+//
+
+import SwiftUI
+
+@main
+struct MelodicStampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
