e2d2fed107922149835441a766c94ca45f86ddfa
KrLite <wang070621@foxmail.com>
2025-01-21 22:37:28 +0800

    Backup

---
+++ Melodic Stamp Tests/Utilities/Extensions/Sequence+Extensions+Tests.swift
@@ -0,0 +1,18 @@
+//
+//  Sequence+Extensions+Tests.swift
+//  Melodic Stamp Tests
+//
+//  Created by KrLite on 2025/1/21.
+//
+
+import Foundation
+@testable import Melodic_Stamp
+import Testing
+
+@Suite struct SequenceTests {
+    @Test func normalize() {
+        let sequence: [Float] = [1, 2, 3, 4, 5]
+        let normalizedSequence: [Float] = [0, 0.25, 0.5, 0.75, 1]
+        #expect(sequence.normalized == normalizedSequence)
+    }
+}
--- Melodic Stamp/Utilities/Extensions/Foundation/Sequence+Extensions.swift
+++ Melodic Stamp/Utilities/Extensions/Foundation/Sequence+Extensions.swift
@@ -10,13 +10,14 @@ import Foundation
 extension Sequence where Element: Comparable {
     var span: ClosedRange<Element>? {
         guard let min = self.min(), let max = self.max() else { return nil }
+        guard min < max else { return nil }
         return min...max
     }
 }
 
 extension Sequence where Element: Comparable & FloatingPoint {
     var normalized: [Element] {
-        guard let span else { return map(\.self) }
+        guard let span else { return map { _ in .zero } }
         return map { value in
             (value - span.lowerBound) / (span.upperBound - span.lowerBound)
         }
