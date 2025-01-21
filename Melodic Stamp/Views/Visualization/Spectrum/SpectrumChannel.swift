4d3dfa2b77073eb5bb4179be1a24b64d05266372
KrLite <wang070621@foxmail.com>
2025-01-21 22:27:25 +0800

    ♻️ Refactor SpectrumChannel

--- Melodic Stamp/Views/Visualization/Spectrum/SpectrumChannel.swift
+++ Melodic Stamp/Views/Visualization/Spectrum/SpectrumChannel.swift
@@ -9,28 +9,54 @@ import SwiftUI
 
 struct SpectrumChannel: View {
     var spectra: [Float]
-    var barWidth: CGFloat
-    var space: CGFloat
-    var bounds: CGSize
+    var barWidth: CGFloat = 2
+    var verticalBaselineFactor: CGFloat = .zero
 
-    private let topSpace: CGFloat = 0.0
-    private let bottomSpace: CGFloat = 0.0
+    @State private var containerSize: CGSize = .zero
 
     var body: some View {
         Path { path in
+            let flippedVerticalBaselineFactor = 1 - verticalBaselineFactor
+            let availableBarWidth = containerSize.width / CGFloat(spectra.count)
+            let verticalBaseline = lerp(0, containerSize.height, factor: flippedVerticalBaselineFactor)
+
             for (i, amplitude) in spectra.enumerated() {
-                let x = CGFloat(i) * (barWidth + space) + space
-                let y = translateAmplitudeToYPosition(amplitude: amplitude, bounds: bounds)
-                let barHeight = bounds.height - y - bottomSpace
-                let bar = CGRect(x: x, y: y, width: barWidth, height: barHeight)
-                path.addRect(bar)
+                let height = height(ofAmplitude: amplitude)
+                let x = (CGFloat(i) + 0.5) * availableBarWidth - barWidth / 2
+                let y = verticalBaseline - flippedVerticalBaselineFactor * height
+
+                let upperBar = CGRect(x: x, y: y, width: barWidth, height: height)
+                path.addRect(upperBar)
             }
         }
-        .frame(width: bounds.width, height: bounds.height)
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .onGeometryChange(for: CGSize.self) { proxy in
+            proxy.size
+        } action: { newValue in
+            containerSize = newValue
+        }
     }
 
-    private func translateAmplitudeToYPosition(amplitude: Float, bounds: CGSize) -> CGFloat {
-        let barHeight = CGFloat(amplitude) * (bounds.height - bottomSpace - topSpace)
-        return bounds.height - bottomSpace - barHeight
+    private func height(ofAmplitude amplitude: Float) -> CGFloat {
+        CGFloat(amplitude) * containerSize.height
     }
+}
+
+#Preview {
+    @Previewable @State var verticalBaselineFactor: CGFloat = .zero
+
+    VStack {
+        SpectrumChannel(
+            spectra: [
+                0.1, 0.4, 0.24, 0.7, 0.45,
+                0.32, 0.88, 0.91, 0.56, 0.4,
+                0.72, 0.66, 0.21, 0.8, 0.23
+            ],
+            verticalBaselineFactor: verticalBaselineFactor
+        )
+        .border(.blue)
+
+        Slider(value: $verticalBaselineFactor)
+    }
+    .padding()
 }
