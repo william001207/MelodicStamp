fd47de00cf0e680996f67d1817e596fb7757be77
KrLite <wang070621@foxmail.com>
2025-01-21 22:11:51 +0800

    ♻️ Refactor SpectrumView

--- Melodic Stamp/Views/Visualization/Spectrum/SpectrumView.swift
+++ Melodic Stamp/Views/Visualization/Spectrum/SpectrumView.swift
@@ -9,83 +9,70 @@ import SwiftUI
 
 struct SpectrumView: View {
     var spectra: [[Float]]
+    var barWidth: CGFloat = 2
 
-    var barWidth: CGFloat = 2.0
-    var space: CGFloat = 2.0
-    private let bottomSpace: CGFloat = 0.0
-    private let topSpace: CGFloat = 0.0
+    @State private var containerSize: CGSize = .zero
 
     var body: some View {
-        VStack(alignment: .center) {
-            GeometryReader { geometry in
-                HStack(spacing: space) {
-                    if let leftSpectra = spectra.first {
-                        let sortedLeft = leftSpectra.sorted()
-                        HStack(spacing: space) {
-                            ForEach(0 ..< 3, id: \.self) { index in
-                                let amplitude = averageAmplitude(for: sortedLeft, index: index, totalBars: 3)
-                                bar(amplitude: amplitude, bounds: geometry.size)
-                                    .frame(width: barWidth)
-                            }
-                        }
-                    }
+        HStack {
+            if let leftSpectra = spectra.first {
+                spectrum(sorted: leftSpectra)
+            }
 
-                    if let rightSpectra = spectra.last {
-                        let sortedRight = Array(rightSpectra.sorted().reversed())
-                        HStack(spacing: space) {
-                            ForEach(0 ..< 3, id: \.self) { index in
-                                let amplitude = averageAmplitude(for: sortedRight, index: index, totalBars: 3)
-                                bar(amplitude: amplitude, bounds: geometry.size)
-                                    .frame(width: barWidth)
-                            }
-                        }
-                    }
-                }
-                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
+            if let rightSpectra = spectra.last {
+                spectrum(sorted: rightSpectra)
+            }
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
+        .onGeometryChange(for: CGSize.self) { proxy in
+            proxy.size
+        } action: { newValue in
+            containerSize = newValue
+        }
+    }
+
+    @ViewBuilder private func spectrum(sorted: [Float]) -> some View {
+        HStack {
+            ForEach(0 ..< 3, id: \.self) { index in
+                let amplitude = averageAmplitude(in: sorted, index: index, count: 3)
+                bar(amplitude: amplitude)
+                    .frame(maxWidth: .infinity)
             }
         }
     }
 
-    @ViewBuilder private func bar(amplitude: Float, bounds: CGSize) -> some View {
+    @ViewBuilder private func bar(amplitude: Float) -> some View {
         VStack {
-            let barHeight = translateAmplitudeToYPosition(amplitude: amplitude, bounds: bounds)
+            let height = height(ofAmplitude: amplitude)
 
-            Rectangle()
-                .fill(Color.black.opacity(0.45))
-                .frame(height: barHeight)
-                .cornerRadius(8)
-                .animation(.easeInOut(duration: 0.15), value: barHeight)
+            Capsule()
+                .frame(width: barWidth, height: height)
+                .animation(.easeInOut(duration: 0.15), value: height)
         }
     }
 
-    private func averageAmplitude(for channel: [Float], index: Int, totalBars: Int) -> Float {
-        let segmentLength = channel.count / totalBars
+    private func averageAmplitude(in data: [Float], index: Int, count: Int) -> Float {
+        let segmentLength = data.count / count
         let start = segmentLength * index
-        let end = min(start + segmentLength, channel.count)
-        let segment = channel[start ..< end]
+        let end = min(start + segmentLength, data.count)
+        let segment = data[start ..< end]
 
         let sum = segment.reduce(0, +)
         let average = sum / Float(segment.count)
-
-        let adjustedHeight = average * 5.0
-        return adjustedHeight
+        return max(0, min(1, average))
     }
 
-    private func translateAmplitudeToYPosition(amplitude: Float, bounds: CGSize) -> CGFloat {
-        let maxHeight = bounds.height
-        let calculatedHeight = CGFloat(amplitude) * maxHeight
-
-        return min(max(calculatedHeight, 2), 20)
+    private func height(ofAmplitude amplitude: Float) -> CGFloat {
+        max(CGFloat(amplitude) * containerSize.height, barWidth)
     }
 }
 
 #Preview {
-    VStack {
-        SpectrumView(spectra: [
-            [0.1, 0.3, 0.5, 0.7, 0.9, 0.2, 0.5, 0.8, 0.3, 0.6],
-            [0.9, 0.7, 0.5, 0.3, 0.1, 0.8, 0.6, 0.4, 0.2, 0.5]
-        ])
-        .frame(width: 20, height: 20)
-    }
-    .background(Color.white)
+    SpectrumView(spectra: [
+        [0.1, 0.3, 0.5, 0.7, 0.9, 0.2, 0.5, 0.8, 0.3, 0.6],
+        [0.9, 0.7, 0.5, 0.3, 0.1, 0.8, 0.6, 0.4, 0.2, 0.5]
+    ])
+    .border(.blue)
+    .frame(width: 100, height: 100)
+    .padding()
 }
